d66694373e2bbb5200917a400b3e3f7099f99ab2
DonNavinS <seneviratnanavin@gmail.com>
2021-09-08 20:49:25 -0400

    Set up conditional rendering when clicking nav bar links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React, { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import Project from "./components/Projects";
 
 function App() {
+  const pages = ["About me", "Projects", "Resume", "Contact"];
+  const [page, changePage] = useState(pages[0]);
   return (
     <div className="App">
       <div>
         <header>
           <h1>Navin Seneviratna Portfolio</h1>
-          <Nav />
+          <Nav pages={pages} page={page} changePage={changePage} />
         </header>
       </div>
       <div>
+        <h1>{page}</h1>
         <h2>Projects</h2>
         <div className="Projects">
           <Project title="Project 1"></Project>
@@ -23,7 +27,17 @@ function App() {
         </div>
       </div>
       <div>
-        <footer></footer>
+        <footer>
+          <a href="https://github.com/DonNavinS">
+            <img src="./assets/images/Github-logo.jpg" alt="github" />
+          </a>
+          <a href="https://www.linkedin.com/in/don-navin-seneviratna/">
+            <img src="./assets/images/LinkedIn-logo.jpg" alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/DonNavinS">
+            <img src="./assets/images/Github-logo.jpg" alt="github" />
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,14 +1,47 @@
 import React from "react";
 
-function Nav() {
+function Nav(props) {
+  const { pages = [], changePage } = props;
   return (
     <div>
       <nav className="nav">
         <ul className="navBar">
-          <li className="navBarLinks">About Me</li>
-          <li className="navBarLinks">Projects</li>
-          <li className="navBarLinks">Resume</li>
-          <li className="navBarLinks">Contact</li>
+          <li className="navBarLinks">
+            <button
+              onClick={() => {
+                changePage(pages[0]);
+              }}
+            >
+              About Me
+            </button>
+          </li>
+          <li className="navBarLinks">
+            <button
+              onClick={() => {
+                changePage(pages[1]);
+              }}
+            >
+              Projects
+            </button>
+          </li>
+          <li className="navBarLinks">
+            <button
+              onClick={() => {
+                changePage(pages[2]);
+              }}
+            >
+              Resume
+            </button>
+          </li>
+          <li className="navBarLinks">
+            <button
+              onClick={() => {
+                changePage(pages[3]);
+              }}
+            >
+              Contact
+            </button>
+          </li>
         </ul>
       </nav>
     </div>
